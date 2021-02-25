d97047fa9ed51d612c0e7f01eb78ae02b89a320e
davidsquines <davidsquines@gmail.com>
2021-02-25 14:49:22 -0800

    Implemented Unit Tests using Selenium

---
+++ selenium/htmlUnitTest.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;	
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.junit.Assert;
+import java.util.List;
+public class htmlUnitTest {				
+
+	public static void main(String[] args) {
+
+		// Creating a new instance of the HTML unit driver
+		WebDriver driver = new HtmlUnitDriver(true); //enable JavaScript
+		              
+		// Navigate to local Jekyll site
+		driver.get("http://localhost:4000");					
+          
+		// This code will test and print the page title
+		String pageTitle = driver.getTitle();
+		try{
+			Assert.assertNotEquals(pageTitle, "");
+		}catch (AssertionError e){
+			System.out.println("Page Title cannot be blank");
+			throw e;
+		}
+		Assert.assertFalse(pageTitle.matches("^\\s*$")); // uses regexp
+		System.out.println("Page title is: " + pageTitle);
+
+		// This code will check the HTML source for Jekyll 404 errors
+		String pageSource = driver.getPageSource();
+		try{
+			Assert.assertFalse(pageSource.contains("404.html"));
+		}catch(AssertionError e){
+			System.out.println("404 Error found");
+			Assert.assertFalse(pageSource.contains("404.html"));
+			throw e;
+		}	
+		Assert.assertFalse(pageSource.contains("requested page could not be found")); 
+
+		//Create a test that is specific to YOUR Jekyll site her
+		List<WebElement> usernames = driver.findElements(By.className("username"));
+		for(WebElement element: usernames){
+
+			String name = element.getText();
+			Assert.assertEquals("dsqsfl",name);
+			System.out.print(name + " ");
+		}
+		System.out.println();
+		driver.quit();			
+	}
+}	
